287fac2e7214712eb04898220d265e55c5bd00b4
Yogesh205 <yogeshrao8500@gmail.com>
2025-02-22 22:48:28 +0530

    Fix: Bind host and port for render deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["e-commerce-website-1-z8s1.onrender.com"], // Update to your new host
+    host: "0.0.0.0", // Explicitly bind server to 0.0.0.0
+    allowedHosts: ["e-commerce-website-6vkc.onrender.com"], // Allowed Hosts
   },
 });
