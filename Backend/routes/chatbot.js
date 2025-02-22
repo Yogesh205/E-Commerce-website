879883d6c6b84821ab705c5c34589a36300aed3f
Yogesh205 <yogeshrao8500@gmail.com>
2025-02-22 15:04:04 +0530

    Fixed port binding issue

--- Backend/routes/chatbot.js
+++ Backend/routes/chatbot.js
@@ -6,7 +6,7 @@ const axios = require("axios");
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 500;
+const port = process.env.PORT || 4000;
 
 // Middleware
 app.use(cors());
