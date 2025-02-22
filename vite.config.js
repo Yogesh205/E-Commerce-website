df5b76cf54b5576c56e8baa07dcef181fb286397
Yogesh205 <yogeshrao8500@gmail.com>
2025-02-22 21:02:26 +0530

    Added allowed hosts in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 3000, // Use Render's assigned port or fallback to 3000
-    host: "0.0.0.0", // Make the server accessible externally (important for deployment)
+    host: true, // Allow access from all hosts (if needed, change to specific domains)
+    allowedHosts: ["e-commerce-website-1-z8s1.onrender.com"], // Add your domain here
   },
 });
